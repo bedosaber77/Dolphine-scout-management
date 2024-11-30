ffb0618c3001465583eb5b824f83e4e461b4d845
Abdelrahman Medhat Saber <136710727+bedosaber77@users.noreply.github.com>
2024-11-30 20:01:17 +0200

    Add API routes for authentication and user registration v1

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
 const db = require("./config/DBmanager");
 const app = express();
-
-db.connect();
+const apiRouter = require("./routes/api");
 
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+if (process.env.NODE_ENV === "dev") app.use(morgan("dev"));
+db.connect();
+
+app.use("/api", apiRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
