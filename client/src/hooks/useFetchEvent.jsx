7e25c3f73f1b8cdf2a388d4cb218e4396fa80180
Omar Nabil <139366121+OmarNabil005@users.noreply.github.com>
2024-12-10 21:07:26 +0200

    merging edits from main

--- client/src/hooks/useFetchEvent.jsx
+++ client/src/hooks/useFetchEvent.jsx
@@ -1,21 +1,36 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const getFetchEvent = (id) => {
   const [event, setEvent] = useState([]);
+  const [leader, setLeader] = useState([]);
+  const [location, setLocation] = useState([]);
 
   useEffect(() => {
     axios
       .get(`http://localhost:3000/api/events/${id}`)
       .then((response) => {
         setEvent(response.data);
+        axios
+          .get(
+            `http://localhost:3000/api/scoutleaders/${response.data.ScoutLeader_ID}`
+          )
+          .then((response) => {
+            setLeader(response.data);
+          });
+        axios
+          .get(
+            `http://localhost:3000/api/locations/${response.data.Location_ID}`
+          )
+          .then((response) => {
+            setLocation(response.data);
+          });
       })
       .catch((error) => {
-        console.error("Error fetching event:", error);
+        console.error('Error fetching event:', error);
       });
   }, []);
-
-  return event;
+  return { ...event, ...leader, ...location };
 };
 
 export default getFetchEvent;
--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,24 +1,25 @@
-import EmblaCarousel from "../components/ui/EmblaCarousel";
-import { useParams } from "react-router-dom";
-import useFetchEvent from "../hooks/useFetchEvent";
-import useFetchLocation from "../hooks/useFetchLocation";
-import "../styles/base.css";
-import "../styles/embla.css";
-import "../styles/eventLayout.css";
+import EmblaCarousel from '../components/ui/EmblaCarousel';
+import { useParams } from 'react-router-dom';
+import useFetchEvent from '../hooks/useFetchEvent';
+import useFetchLocation from '../hooks/useFetchLocation';
+import '../styles/base.css';
+import '../styles/embla.css';
+import '../styles/eventLayout.css';
 
 const IMAGES = [
-  "https://plus.unsplash.com/premium_photo-1661893427047-16f6ddc173f6?q=80&w=500&h=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1541790453029-41ad91024de6?w=500&h=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8a2F5YWt8ZW58MHx8MHx8fDA%3D",
-  "https://images.unsplash.com/photo-1450500392544-c2cb0fd6e3b8?w=500&h=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8a2F5YWt8ZW58MHx8MHx8fDA%3D",
+  'https://plus.unsplash.com/premium_photo-1661893427047-16f6ddc173f6?q=80&w=500&h=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1541790453029-41ad91024de6?w=500&h=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8a2F5YWt8ZW58MHx8MHx8fDA%3D',
+  'https://images.unsplash.com/photo-1450500392544-c2cb0fd6e3b8?w=500&h=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8a2F5YWt8ZW58MHx8MHx8fDA%3D',
 ];
 
 const OPTIONS = {
-  direction: "rtl", // right to Left
+  direction: 'rtl', // right to Left
 };
 const Event = () => {
   const { id } = useParams();
   const event = useFetchEvent(id);
-  const { LocationName } = useFetchLocation(event.Location_ID);
+  const gathering = useFetchGathering(id);
+  const camp = useFetchCamp(id);
   if (event.length === 0) {
     return <p>event not found</p>;
   } else {
@@ -30,16 +31,17 @@ const Event = () => {
             <ul>
               <li>{event.Ename}</li>
               <li>
-                {new Date(event.Edate).toLocaleDateString("ar-EG", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
+                التاريخ:
+                {new Date(event.Edate).toLocaleDateString('ar-EG', {
+                  year: 'numeric',
+                  month: 'long',
+                  day: 'numeric',
                 })}
               </li>
-              <li>{LocationName}</li>
-              <li>{event.ScoutLeader_ID}</li>
+              <li>المكان: {event.LocationName}</li>
+              <li>القائد: {event.name}</li>
               <li>حضرت: ×</li>
-              <li>{event.Budget}</li>
+              <li>الميزانية: {event.Budget}</li>
             </ul>
             <button className="attendance-button">سجل الحضور</button>
           </div>
