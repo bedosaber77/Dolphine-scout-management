9743797b19c8353b020221296a8108b27a5fd970
Omar Nabil <139366121+OmarNabil005@users.noreply.github.com>
2024-12-17 20:23:23 +0200

    feat: get unverifie users query

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,6 +18,23 @@ exports.getAllUsers = async (req, res) => {
   }
 };
 
+exports.getUnverifiedUsers = async (req, res) => {
+  try {
+    const query = `SELECT * FROM "User where "role" IS NULL"`;
+    const params = [];
+    const result = await db.query(query, params);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'no users found' });
+    }
+
+    return res.json(result.rows);
+  } catch (error) {
+    console.log('Error executing query', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 exports.getUserById = async (req, res) => {
   const { id } = req.params;
   try {
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,6 +4,7 @@ const { ValidateAddUser } = require('../middlewares/Validate');
 const Router = express.Router();
 
 Router.get('/', userController.getAllUsers);
+Router.get('/unverified', userController.getUnverifiedUsers);
 Router.get('/:id', userController.getUserById);
 
 Router.post('/', ValidateAddUser, userController.addUser);
